d16fb74fd4dd1cc891e5f26e084956b451cab54d
shukuratgithubs <shukroh74@gmail.com>
2022-09-04 12:34:18 -0700

    console.py

--- console.py
+++ console.py
@@ -159,32 +159,21 @@ class HBNBCommand(cmd.Cmd):
                 if ob_name == args[0] and ob_id == args[1].strip('"'):
                     if len(args) == 2:
                         print("** attribute name missing **")
-                 return False
-        if len(argl) == 3:
-            try:
-                type(eval(argl[2])) != dict
-            except NameError:
-                print("** value missing **")
-                return False
+                 elif len(args) == 3:
+                         print("** value missing **")
+                        else:
+                         setattr(objc, args[2], args[3])
+                         storage.save()
+                     return
+                print("** no instance found **")
 
-        if len(argl) == 4:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            if argl[2] in obj.__class__.__dict__.keys():
-                valtype = type(obj.__class__.__dict__[argl[2]])
-                obj.__dict__[argl[2]] = valtype(argl[3])
-            else:
-                obj.__dict__[argl[2]] = argl[3]
-        elif type(eval(argl[2])) == dict:
-            obj = objdict["{}.{}".format(argl[0], argl[1])]
-            for k, v in eval(argl[2]).items():
-                if (k in obj.__class__.__dict__.keys() and
-                        type(obj.__class__.__dict__[k]) in {str, int, float}):
-                    valtype = type(obj.__class__.__dict__[k])
-                    obj.__dict__[k] = valtype(v)
-                else:
-                    obj.__dict__[k] = v
-        storage.save()
+     def do_quit(self, line):
+         """ Quit command to exit the command interpreter """
+         return True
 
+    def do_EOF(self, line):
+         """ EOF command to exit the command interpreter """
+         return True
 
-if __name__ == "__main__":
-    HBNBCommand().cmdloop()    
+ if __name__ == '__main__':
+     HBNBCommand().cmdloop()    
